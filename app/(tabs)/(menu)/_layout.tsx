c9b4c60f453708a875f9e0e237c8b0a1f36ec1dc
Anitor <anitorabraham@gmail.com>
2024-07-26 15:14:39 +0100

    feat: added menu page

---
+++ app/(tabs)/(menu)/_layout.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+
+const MenuLayout = () => {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
+};
+
+export default MenuLayout;
