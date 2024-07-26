3001616dbc6fdb4a6b3c6a04a19eaf67c46a835a
PreciousExpectoo <n.precious@expectoo.ng>
2024-07-26 15:12:20 +0100

    initial-auth-push

---
+++ app/(tokenValidation)/index.tsx
@@ -0,0 +1,67 @@
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+  Image,
+  TextInput,
+} from "react-native";
+import CustomButton from "@/components/ui/CustomButton";
+import { useState } from "react";
+import { router } from "expo-router";
+
+const index = () => {
+  const [token, setToken] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleContinue = () => {
+    if (token !== "000") {
+      setErrorMessage("Please Input a correct token.");
+    } else {
+      setErrorMessage("");
+      router.push("/onBoarding");
+    }
+  };
+
+  return (
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView className="w-full h-full flex-col  relative">
+        <View className="px-4 flex-col space-y-[25px] top-[50%]">
+          <View className="w-full flex-row justify-center">
+            <Image source={require("@/assets/images/logo.png")} />
+          </View>
+          <Text className="text-center p-4 bg-[#F8F8F8] rounded-xl">
+            Loremm ipsum dolor sit amet consectetur. Congue magna arcu sapien
+            sed duis dolor enim.
+          </Text>
+          <View className="flex-col space-y-[25px]">
+            <View className="w-full flex-row justify-center bg-[#F8F8F8] p-4 rounded-xl">
+              <TextInput
+                placeholder="Input Company Token"
+                placeholderTextColor="#000000"
+                className="w-full text-center"
+                value={token}
+                onChangeText={setToken}
+              />
+            </View>
+            {errorMessage ? (
+              <Text className="text-red-500 text-center mt-2">
+                {errorMessage}
+              </Text>
+            ) : null}
+            <View>
+              <CustomButton
+                title="Continue"
+                onPress={handleContinue}
+                customStyle="bg-[#192655]"
+                textStyle="text-white"
+              />
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default index;
---
+++ app/(tokenValidation)/onBoarding.tsx
@@ -0,0 +1,35 @@
+import { SafeAreaView, ScrollView, View, Text } from "react-native";
+import { Image } from "react-native";
+import CustomButton from "@/components/ui/CustomButton";
+
+const OnBoarding = () => {
+  return (
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView className="w-full h-full flex-col  relative">
+        <View className="px-4  space-y-[150px] top-[50%] ">
+          <View className="w-full flex-row justify-center">
+            <Image source={require("@/assets/images/onLogo.png")} />
+          </View>
+          <View className="w-full space-y-[15px]">
+            <View>
+              <CustomButton
+                title="Login"
+                textStyle="text-white"
+                customStyle="bg-[#192655]"
+              />
+            </View>
+            <View>
+              <CustomButton
+                title="Sign Up"
+                textStyle="text-[#192655]"
+                customStyle="bg-[#ffffff] border-[1px] border-solid border-[#192655]"
+              />
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default OnBoarding;
